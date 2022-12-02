1c5b7c522e14735c4b528f6c9b0d00c49b547a14
310354 <103193275+310354@users.noreply.github.com>
2022-12-02 18:42:30 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+//https://teachablemachine.withgoogle.com/models/qh36cod4T/model.json
+Prediction_1="";
+Prediction_2="";
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format:'png',
+    png_quality:90
+});
+
+camera=document.getElementById("camera");
+Webcam.attach('#camera');
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="captured_image"src="'+ data_uri + '"/>'
+    });
+}
+
+console.log('ml5 version',ml5.version);
+classifier=ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/qh36cod4T/model.json',modelLoaded);
+
+function modelLoaded()
+{
+    console.log('modelLoaded');
+}
+
+function Speak()
+{
+    var synth=window.speechSynthesis;
+    Speak_data_1="The first prediction is" + Prediction_1;
+    Speak_data_2="The second prediction is" + Prediction_2;
+    var utterthis=new SpeechSynthesisUtterance(Speak_data_1+Speak_data_2);
+    synth.speak(utterthis);
+}
